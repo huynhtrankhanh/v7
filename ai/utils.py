357bb1db810c3ad7775098b60f08cae6c3aee936
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2026-01-11 10:08:45 +0700

    Revert "Minimize repository to core Syllable and Regex logic"

---
+++ ai/utils.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn.functional as F
+import time
+from utils.preprocess import standardize_data
+from .tokenizer import tokenizer
+from .configs import MAX_SEQUENCE_LEN, DEVICE, BASE_MODEL_CHECKPOINT_PATH, MODEL_SIZES
+from .model import GPT, GPTConfig
+import builtins
+
+def next(model: GPT, seqs: list[str]) -> list[list[int]]:
+    # Standardize and tokenize the input sequences
+    seqs = [standardize_data(seq).lower() for seq in seqs]
+    tokens = [tokenizer.tokenize(seq.split())[-MAX_SEQUENCE_LEN:] for seq in seqs]
+    tokens = torch.tensor(tokens, dtype=torch.long).to(builtins.next(model.parameters()).device)
+
+    with torch.no_grad():
+        # Forward pass through the model
+        logits = model(tokens)[0]  # (B, T, vocab_size)
+        
+        # Take logits at the last position
+        logits = logits[:, -1, :]  # (B, vocab_size)
+        
+        # Get the indices that would sort the logits in descending order
+        sorted_indices = torch.argsort(logits, dim=-1, descending=True)
+        
+        # Convert to list of indices
+        sorted_indices_list = sorted_indices.cpu().tolist()
+    
+    return sorted_indices_list
+
+def generate(model: GPT, seq: str, n: int):
+    tokens = tokenizer.tokenize(seq.split())
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0)
+    
+    x = tokens.to(DEVICE)
+    
+    generated_tokens = []
+    
+    for _ in range(n):
+        with torch.no_grad():
+            logits = model(x)[0] # (B, T, vocab_size)
+
+            # Take logits at the last position
+            logits = logits[:, -1, :] # (B, vocab_size)
+            probs = F.softmax(logits, dim=-1) 
+
+            # Take just topk is 50
+            topk_probs, topk_indices = torch.topk(probs, 50, dim=-1) # (B, 50)
+
+            ix = torch.multinomial(topk_probs, 1) # (B, 1)
+            xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+            
+            generated_tokens.append(xcol[0][0].tolist())
+
+            x = torch.cat((x, xcol), dim=1)
+            x = x[:, -MAX_SEQUENCE_LEN:] # take just the last MAX_SEQUENCE_LEN tokens
+            
+    return tokenizer.detokenize(generated_tokens)
+
+def get_model(model_size='base', checkpoint_path=BASE_MODEL_CHECKPOINT_PATH, verbose=1):
+    if not checkpoint_path:
+        checkpoint_path = BASE_MODEL_CHECKPOINT_PATH
+        
+    model = GPT(
+        GPTConfig(**MODEL_SIZES[model_size])
+    )
+    
+    try:
+        checkpoint = torch.load(checkpoint_path, map_location=torch.device(DEVICE))
+        # Remove 'module.' prefix from all keys in the checkpoint state_dict
+        checkpoint_state_dict = checkpoint['model_state_dict'] if 'model_state_dict' in checkpoint else checkpoint
+        new_state_dict = {}
+        
+        for key, value in checkpoint_state_dict.items():
+            new_key = key.replace('module.', '')  # Remove 'module.' prefix
+            new_state_dict[new_key] = value
+        
+        model.load_state_dict(new_state_dict)
+    
+    except Exception as e:
+        print(e)
+        
+        try:
+            # If direct load fails, attempt without the 'model_state_dict' key
+            model.load_state_dict(checkpoint)
+        except Exception as e:
+            raise Exception(f"Failed to load checkpoint from {checkpoint_path}: {e}")
+    
+    if verbose:
+        print(f"\tCheckpoint path: {checkpoint_path}")
+        total_params = sum(p.numel() for p in model.parameters())
+        print(f"\tTotal number of parameters: {total_params}")
+        
+    model.eval()
+    
+    return model
---
+++ sandbox/overview.py
@@ -0,0 +1,83 @@
+import sys, os, time, json
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.vietnamese import Vietnamese, Alphabet
+from utils.dictionary import Dictionary
+import utils.preprocess as preprocess
+import time
+import numpy as np
+import statistics
+import json
+
+def main():
+    total_words = []
+    replicate_sounds = 0
+    none_sounds = 0
+    
+    Cs = len(Vietnamese.consonant_families)
+    Rs = len(Vietnamese.rhymes_families)
+    ERs = len(list(filter(lambda rhyme: rhyme[-1] in ['n', 'g', 'm', 'h'], Vietnamese.rhymes_families)))
+    nERs = Rs - ERs
+    print()
+    print(f"Cs:    {Cs} (consonant families)")
+    print(f"Rs:    {Rs} (rhyme families)")
+    print(f"ERs:   {ERs} (enterable rhymes)")
+    print(f"nERs:  {nERs} (non-enterable rhymes)")
+    print()
+    print(f"CERs:  {Cs*ERs*8}")
+    print(f"CnERs: {Cs*nERs*6}")
+    
+    print()
+    print(f"Loop for each consonant family and each rhyme family")
+    # Count how many Vietnamese distinc words
+    for c in Vietnamese.consonant_families:
+        for r in Vietnamese.rhymes_families:
+            wordss = Vietnamese.word_with_tones(c, r)
+                
+            if wordss is None:
+                print(f"\tNone case:     ({c}, {r}) -> {wordss}")
+                none_sounds += 8
+                
+                continue
+            
+            if len(wordss) > 1:
+                print(f"\tMultiple case: ({c}, {r}) -> {wordss}")
+                replicate_sounds += len(wordss[1])
+            
+            wordss = [word for words in wordss for word in words]
+            total_words += wordss
+    
+    # total_words is sure distinct
+
+    print()
+    print(f"Total distict words:  {len(total_words)}")
+    print(f"Total distict sounds: {len(total_words) - replicate_sounds}") # equal to Cs*ERs*8 + Cs*nERs*6 - none_sounds
+    
+    sounds = []
+    with open("checkpoints/db.json", 'r') as f:
+        db = json.load(f)
+        for c in db:
+            for r in db[c]:
+                for t in db[c][r]:
+                    sounds += [obj['value'] for obj in db[c][r][t]]
+    
+    enum = {}
+    for no, sound in enumerate(sounds, start=1):
+        enum[sound] = no
+    # save enum to checkpoints/enum.json
+    with open("checkpoints/enum.json", 'w') as f:
+        json.dump(enum, f, indent=2)    
+    
+    # print('---')
+    # for word in total_words:
+    #     if word not in sounds:
+    #         print(word)
+    # print('---')
+    # for word in sounds:
+    #     if word not in total_words:
+    #         print(word)
+    # print('---')
+        
+                 
+if __name__ == "__main__":
+    main()
