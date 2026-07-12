278db91bc255ca285f09936668b86d152284f56e
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2026-07-12 09:34:23 +0700

    Create evaluateInference.ts with evaluate function
    Added evaluate function stub for inference evaluation.

---
+++ evaluateInference.ts
@@ -0,0 +1,6 @@
+// This code returns a score indicating how bad the inference algorithm is.
+// The lower the score, the better the inference quality.
+
+const evaluate = async (text: string, inference: (request: string[]) => string[]): number => {
+  
+};
